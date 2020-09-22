9ad78d704323f873af8edd713960998cd505cfb8
Richard Kasperowski <r@kasperowski.com>
2020-09-22 19:30:18 -0400

    Initial commit

---
+++ GildedRoseTest.cs
@@ -0,0 +1,17 @@
+﻿using NUnit.Framework;
+using System.Collections.Generic;
+
+namespace GildedRose_Refactoring_Kata_csharpcore
+{
+    public class GildedRoseTest
+    {
+        [Test]
+        public void foo()
+        {
+            IList<Item> Items = new List<Item> { new Item { Name = "foo", SellIn = 0, Quality = 0 } };
+            GildedRose app = new GildedRose(Items);
+            app.UpdateQuality();
+            Assert.AreEqual("fixme", Items[0].Name);
+        }
+    }
+}
---
+++ Item.cs
@@ -0,0 +1,9 @@
+﻿namespace GildedRose_Refactoring_Kata_csharpcore
+{
+    public class Item
+    {
+        public string Name { get; set; }
+        public int SellIn { get; set; }
+        public int Quality { get; set; }
+    }
+}
